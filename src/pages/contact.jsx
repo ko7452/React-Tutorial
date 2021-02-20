9e6d1909681cc441ab0d6139396b61ce2ba24f04
KoYunHyuk <sksgur3217@gmail.com>
2021-02-20 20:14:33 +0900

    complated setting pages

---
+++ src/pages/contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// react안에 Component를 받아와 class component를 만들어준다
+class Contact extends React.Component {
+  render() {
+    return <div>Contact</div>;
+  }
+}
+
+export default Contact;
---
+++ src/pages/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// react안에 Component를 받아와 class component를 만들어준다
+class Home extends React.Component {
+  render() {
+    return <div>Home</div>;
+  }
+}
+
+export default Home;
