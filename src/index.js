dd0b1a6a817fb13b976129a522e8bdef0c94edee
KoYunHyuk <sksgur3217@gmail.com>
2021-02-20 20:22:55 +0900

    complated setting in index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // App.jsx, 확장자 버전으로 import 해준다.
 import App from './App.jsx';
+// BrowserRouter를 react-router-dom으로부터 불러온다
+import { BrowserRouter } from 'react-router-dom';
 
+// 바뀌어야 되는 페이지인 <App />를 BrowserRouter로 감싸준다
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root'),
 );
